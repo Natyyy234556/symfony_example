4f1a13824cb2567076a0c0f0516f427aeb267d5b
Nataly Rivas <ar21077@ues.edu.sv>
2023-10-21 22:12:06 -0600

    add staged changes

--- config/bundles.php
+++ config/bundles.php
@@ -2,13 +2,10 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-<<<<<<< HEAD
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
-=======
->>>>>>> c727633 (Add initial set of files)
 ];
